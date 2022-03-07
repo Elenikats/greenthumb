ba489040e0a71f6b9a08bf440a6b58a5bb53b0c8
Angela Herrig <angela.k.herrig@gmail.com>
2022-03-07 23:57:32 +0100

    created template for cart and cartOverlay

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,3 +1,111 @@
+import { useState } from "react";
+import plants from "../../plants.js";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import "./cart.scss";
+import CartOverlay from "./CartOverlay.jsx";
+
 export default function Cart() {
-  return <h1>Cart</h1>;
+  const [quantity, setQuantity] = useState(2);
+  const [open, setOpen] = useState(false);
+
+  function removeItem() {}
+
+  function decreaseQuantity() {
+    setQuantity(quantity - 1);
+  }
+
+  function increaseQuantity() {}
+
+  // function handleSubmit() {
+  //   setOpen(true);
+  //   console.log(open);
+  //   return <CartOverlay open={open} />;
+  // }
+
+  if (!open) {
+    return (
+      <Container fluid="lg">
+        <h1>Cart</h1>
+        <Row>
+          <Col md={8}>
+            <Table>
+              <tbody>
+                {plants.map((plant) => {
+                  return (
+                    <tr>
+                      <td className="removeBtn" onClick={removeItem}>
+                        x
+                      </td>
+                      <td>
+                        <img
+                          src={plant.image}
+                          alt={plant.name}
+                          style={{ width: "100px" }}
+                        />
+                      </td>
+                      <td>
+                        <span>{plant.name}</span>
+                      </td>
+                      <td>
+                        <span>{plant.price} €</span>
+                      </td>
+                      <td>
+                        <span
+                          className="quantity count"
+                          onClick={decreaseQuantity}
+                        >
+                          -
+                        </span>
+                        <span className="quantity">{quantity}</span>
+                        <span
+                          className="quantity count"
+                          onClick={increaseQuantity}
+                        >
+                          +
+                        </span>
+                      </td>
+                      <td>
+                        <span>{plant.price * quantity} €</span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
+
+          <Col md={4}>
+            <Table>
+              <tbody className="summary">
+                <tr>
+                  <th>subtotal:</th>
+                  <td>300 €</td>
+                </tr>
+                <tr>
+                  <th>shipping:</th>
+                  <td>7.50 €</td>
+                </tr>
+                <tr>
+                  <th>TOTAL:</th>
+                  <td>307.50 €</td>
+                </tr>
+              </tbody>
+            </Table>
+            <div className="shippingDetails">
+              plants will be sent to:
+              <p>name</p>
+              <p>adress</p>
+            </div>
+
+            <Button onClick={() => setOpen(!open)}>submit</Button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+  return <CartOverlay open={open} />;
 }
--- src/plants.js
+++ src/plants.js
@@ -5,50 +5,55 @@ import plant4 from "./plant-photos/plants/alocasia-zebrina.jpeg";
 import plant5 from "./plant-photos/plants/calathea.jpeg";
 
 const plants = [
-        {
-        id : 1,
-        name : "Monstera Deliciosa var. Borsigiana Variegata",
-        image: plant1,
-        size : "medium",
-        price: "300 €",
-        description : "Variegated Monstera Deliciosa var. Borsigiana, a.k.a Monstera albo, with white variegated leaves is probably one of the most sought after plants these days. We have only got a handful of them as they are very hard to get."
-    },
+  {
+    id: 1,
+    name: "Monstera Deliciosa var. Borsigiana Variegata",
+    image: plant1,
+    size: "medium",
+    price: "300",
+    description:
+      "Variegated Monstera Deliciosa var. Borsigiana, a.k.a Monstera albo, with white variegated leaves is probably one of the most sought after plants these days. We have only got a handful of them as they are very hard to get.",
+  },
 
-    {
-        id : 2,
-        name : ` Alocasia Micholitziana ‘Frydek’ `,
-        image : plant2,
-        size : "small",
-        price : "25",
-        description : "Alocasia Micholitziana ‘Frydek’ is a rare, glamorous specimen with an elegant touch. The soft velvet leaf texture is complimented by the iconic arrowhead shape. What makes this alocasia unique are the sharp, white veins that dissect the deep green leaves. This is a must have for all true plant lovers."
-    },
+  {
+    id: 2,
+    name: ` Alocasia Micholitziana ‘Frydek’ `,
+    image: plant2,
+    size: "small",
+    price: "25",
+    description:
+      "Alocasia Micholitziana ‘Frydek’ is a rare, glamorous specimen with an elegant touch. The soft velvet leaf texture is complimented by the iconic arrowhead shape. What makes this alocasia unique are the sharp, white veins that dissect the deep green leaves. This is a must have for all true plant lovers.",
+  },
 
-    {
-        id : 3,
-        name : "Alocasia Portodora",
-        image : plant3,
-        size : "medium",
-        price : "15",
-        description : "Alocasia Portodora has playful lush green arrowhead-shaped leaves. It can grow quite big so make some space. It requires bright indirect light and high humidity."
-    },
+  {
+    id: 3,
+    name: "Alocasia Portodora",
+    image: plant3,
+    size: "medium",
+    price: "15",
+    description:
+      "Alocasia Portodora has playful lush green arrowhead-shaped leaves. It can grow quite big so make some space. It requires bright indirect light and high humidity.",
+  },
 
-    {
-        id : 4,
-        name : "Alocasia Zebrina",
-        image : plant4,
-        size : "medium",
-        price : "15",
-        description : "Alocasia Zebrina has unique striped stems and arrowhead leaves. An amazing plant for everyone who loves zebras and plants"
-    },
+  {
+    id: 4,
+    name: "Alocasia Zebrina",
+    image: plant4,
+    size: "medium",
+    price: "15",
+    description:
+      "Alocasia Zebrina has unique striped stems and arrowhead leaves. An amazing plant for everyone who loves zebras and plants",
+  },
 
-    {
-        id : 5,
-        name : "Calathea Lancifolia (Rattlesnake Plant)",
-        image : plant5,
-        size : "big",
-        price : "12",
-        description : "Calathea Lancifolia, commonly known a Rattlesnake Plant is a perfect plant for a pattern lover! It has bright green wavy leaves with purple underside that are adorned with alternating ovals of dark green."
-    }
-]
+  {
+    id: 5,
+    name: "Calathea Lancifolia (Rattlesnake Plant)",
+    image: plant5,
+    size: "big",
+    price: "12",
+    description:
+      "Calathea Lancifolia, commonly known a Rattlesnake Plant is a perfect plant for a pattern lover! It has bright green wavy leaves with purple underside that are adorned with alternating ovals of dark green.",
+  },
+];
 
-export default plants
+export default plants;
